9a6ee1bdc3bb6518edff9ed6dfa2994b6f4dfff0
Nikhil-Kumar-Project <nikhil.kumar.123923@gmail.com>
2026-02-21 22:41:14 +0530

    Add files via upload

---
+++ NumberGuessingGame.py
@@ -0,0 +1,20 @@
+import random
+print("You have to guess a number between 1-10")
+cumputer_choice=random.randint(1,10)
+attempt=1
+while True:
+    user_guess=int(input("Enter your guess (if you want to exit enter 0):"))
+    if user_guess==0:
+        print("Thank you!")
+        break
+    elif user_guess==cumputer_choice:
+        print("Congratulation! You guess it right in",attempt,"attempt")
+        break
+    else:
+        if user_guess>cumputer_choice:
+            print("HINT: Guess it more low.")
+            attempt+=1
+        elif user_guess<cumputer_choice:
+            print("HINT: Guess it more high")
+            attempt+=1
+        
